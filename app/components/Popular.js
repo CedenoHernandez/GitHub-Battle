792e572f36b4a64916239cb450fe6af3ffed1a5a
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2019-10-19 10:51:50 -0700

    Add flex box style to homepage

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -47,6 +47,26 @@ function ReposGrid({ repos }) {
                         <h2 className='center-text'>
                             <a className='link' href={html_url}>{login}</a>
                         </h2>
+                        <ul className='card-list'>
+                            <li>
+                                <FaUser color='rgb(255, 191, 116)' size={22} />
+                                <a href={`https://github.com/${login}`}>
+                                    {login}
+                                </a>
+                            </li>
+                            <li>
+                                <FaStar color='rgb(255, 215, 0)' size={22} />
+                                {stargazers_count.toLocaleString()} stars
+                            </li>
+                            <li>
+                                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                                {forks.toLocaleString()} forks
+                            </li>
+                            <li>
+                                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                                {open_issues.toLocaleString()} open
+                            </li>
+                        </ul>
                     </li>
                 )
             })}
